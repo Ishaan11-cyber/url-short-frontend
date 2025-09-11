81b5974ec913c69e4cce12e0f40fbad5c8eda71c
Ishaan Rajput <ishaanrajput05@gmail.com>
2025-09-11 21:41:33 +0530

    Fix: added React logo to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import ReactLogo from './react.svg';
 
 import { getApps } from './utils/helper'
 
@@ -9,6 +10,9 @@ function App() {
 
   return (
     <>
+      <div className="logo-container">
+        <img src={ReactLogo} alt="React Logo" width={100} />
+      </div>
      <Router>
       <CurrentApp />
      </Router>
